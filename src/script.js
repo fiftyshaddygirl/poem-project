4a109a864a879c8c168cbd2cd4155eee67349f06
cynkzn <cynkazan@gmail.com>
2025-04-13 21:40:52 +1000

    adding AI integration with forms

--- src/script.js
+++ src/script.js
@@ -2,16 +2,32 @@
 making sure that every time we click on submit the form, 
 we extract the info in the instruction input*/
 
-function generatePoem(event) {
-  event.preventDefault();
-  let poem = "A crimson blush, a velvet grace";
-
+function displayPoem(response) {
+  console.log(response.data.answer);
+  let poem = response.data.answer;
+  poem = poem.replace("html", "").replace("```", "");
   new Typewriter("#poem", {
     strings: poem,
     autoStart: true,
     cursor: null,
     delay: 1,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  // build the API URL
+  // make a call to API using axios
+  // Display generated poem
+  let instruction = document.querySelector("#topic-input");
+  let apiKey = "940836f57095o1a5bt3c042d91bb08f9";
+  let context =
+    "You are an expert in romance poetry and love to write poem. please generate 4 line poem in HTML basic format and separate each sentance with <br> and make sure to follow user instruction";
+  let prompt = `user instruction: generate short poem about ${instruction.value} and sign Shecodes at the very end`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log("Generating poem");
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemForm = document.querySelector("#form-generator");
